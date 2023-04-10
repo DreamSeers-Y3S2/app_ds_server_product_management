5ac487d2c60a68efe82d63f8a4b40defb6370af1
ThaminduChankana <thaminduchankana@gmail.com>
2023-04-10 16:06:28 +0530

    created server file

--- server.js
+++ server.js
@@ -3,16 +3,23 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
 app.use(express.json());
 app.use("*", cors());
 
-app.get("/product", (req, res) => {
-	res.send("API is Running");
+app.get("/", (req, res) => {
+	res.send("Product API is Running");
 });
 
-const PORT = process.env.PORT || 5002;
+app.use("/product", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, console.log(`Product Server Started on port ${PORT}..`));
