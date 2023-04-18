38e1d53d064f45126676a56bed909cb46a50c99e
Thamindu Chankana Gamage <73847570+ThaminduChankana@users.noreply.github.com>
2023-04-18 23:07:26 +0530

    removed cors consts

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const app = express();
 const productRoutes = require("./routes/productRoutes");
 const connectDB = require("./config/db");
@@ -10,7 +8,6 @@ const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 connectDB();
 app.use(express.json());
-app.use("*", cors());
 
 app.get("/", (req, res) => {
 	res.send("Product API is Running");
@@ -21,5 +18,5 @@ app.use("/products", productRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, console.log(`Product Server Started on port ${PORT}..`));
